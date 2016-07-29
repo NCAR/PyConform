e3eba1d4d45ed1be61327ef46a550ef53e3e8391
Kevin Paul <kpaul@ucar.edu>
2016-07-29 16:27:41 -0600

    Finished: EvalDataNode, ReadDataNode, ValidateDataNode, SetDataNode

--- source/pyconform/dataflows.py
+++ source/pyconform/dataflows.py
@@ -16,14 +16,23 @@ COPYRIGHT: 2016, University Corporation for Atmospheric Research
 LICENSE: See the LICENSE.rst file for details
 """
 
-from numpy import index_exp
-from numpy.ma import MaskedArray, asarray
+from __future__ import print_function
+from abc import ABCMeta, abstractmethod
+from pyconform.indexing import index_str, join
 from cf_units import Unit
 from inspect import getargspec, isfunction
 from os.path import exists
 from netCDF4 import Dataset
+from sys import stderr
 
-from pyconform.indexing import index_str, join
+import numpy
+
+
+#===============================================================================
+# warning - Helper function
+#===============================================================================
+def warning(*objs):
+    print("WARNING:", *objs, file=stderr)
 
 
 #===================================================================================================
@@ -45,7 +54,7 @@ class DimensionsError(ValueError):
 #===================================================================================================
 # DataArray
 #===================================================================================================
-class DataArray(MaskedArray):
+class DataArray(numpy.ma.MaskedArray):
     """
     A DataArray is the basic object passed along the edges of a Data Flow graph
     
@@ -54,7 +63,7 @@ class DataArray(MaskedArray):
     """
 
     def __new__(cls, inarray, units=None, dimensions=None):
-        obj = asarray(inarray).view(cls)
+        obj = numpy.ma.asarray(inarray).view(cls)
 
         if units is None:
             if 'units' not in obj._optinfo:
@@ -105,6 +114,39 @@ class DataNode(object):
     through the __getitem__ interface (i.e., DataNode[item]), returning a slice of a
     DataArray.
     """
+    
+    __metaclass__ = ABCMeta
+
+    def __init__(self, label, *inputs):
+        """
+        Initializer
+        
+        Parameters:
+            label: A label to give the DataNode
+            inputs (tuple): DataNodes that provide input into this DataNode
+        """
+        self._label = label
+        self._inputs = inputs
+
+    @abstractmethod
+    def __getitem__(self, index):
+        """
+        Compute and retrieve the data associated with this DataNode operation
+        """
+        pass
+
+    @property
+    def label(self):
+        return self._label
+
+
+#===================================================================================================
+# EvalDataNode
+#===================================================================================================
+class EvalDataNode(DataNode):
+    """
+    The basic class for evaluating a function on input from neighboring DataNodes
+    """
 
     def __init__(self, label, func, *args):
         """
@@ -115,8 +157,7 @@ class DataNode(object):
             func (callable): A callable function associated with the DataNode operation
             args (tuple): Arguments to the function given by 'func'
         """
-        self._label = label
-
+        # Check func parameter
         if callable(func):
             if hasattr(func, '__call__') and isfunction(func.__call__):
                 argspec = getargspec(func.__call__)
@@ -125,6 +166,7 @@ class DataNode(object):
         else:
             raise TypeError('Function argument to DataNode {} is not callable'.format(label))
 
+        # Check the function arguments
         max_len = len(argspec.args)
         if argspec.defaults is None:
             min_len = max_len
@@ -136,30 +178,30 @@ class DataNode(object):
         if len(args) > max_len:
             raise ValueError(('Too many arguments supplied for DataNode function. '
                               '({} needed, {} supplied)').format(max_len, len(args)))
+
+        # Save the function reference
         self._function = func
-        self._arguments = args
+        
+        # Call the base class initialization
+        super(EvalDataNode, self).__init__(label, *args)
 
     def __getitem__(self, index):
         """
         Compute and retrieve the data associated with this DataNode operation
         """
-        if len(self._arguments) == 0:
+        if len(self._inputs) == 0:
             data = self._function()
             return data[index] if isinstance(data, DataArray) else data
         else:
             args = tuple(d[index] if isinstance(d, (DataArray, DataNode)) else d
-                         for d in self._arguments)
+                         for d in self._inputs)
             return self._function(*args)
-
-    @property
-    def label(self):
-        return self._label
-
+        
 
 #===================================================================================================
-# DataNode
+# ReadDataNode
 #===================================================================================================
-class ReaderNode(DataNode):
+class ReadDataNode(DataNode):
     """
     Basic class for reading data from a NetCDF file
     """
@@ -183,30 +225,21 @@ class ReaderNode(DataNode):
             raise OSError('File path not found: {!r}'.format(filepath))
         self._filepath = filepath
 
-        # Attempt to open the NetCDF file
-        try:
-            ncfile = Dataset(self._filepath, 'r')
-        except:
-            raise OSError('Cannot open as a NetCDF file: {!r}'.format(self._filepath))
-
-        # Parse variable name
-        if not isinstance(variable, basestring):
-            raise TypeError('Unrecognized variable name object of type '
-                            '{!r}: {!r}'.format(type(variable), variable))
-        if variable not in ncfile.variables:
-            raise OSError('Variable {!r} not found in NetCDF file: '
-                          '{!r}'.format(variable, self._filepath))
+        # Attempt to open the NetCDF file to parse the variable name
+        with Dataset(self._filepath, 'r') as ncfile:
+            if not isinstance(variable, basestring):
+                raise TypeError('Unrecognized variable name object of type '
+                                '{!r}: {!r}'.format(type(variable), variable))
+            if variable not in ncfile.variables:
+                raise OSError('Variable {!r} not found in NetCDF file: '
+                              '{!r}'.format(variable, self._filepath))
         self._variable = variable
-
+    
         # Parse the reading index
-        self._shape = ncfile.variables[variable].shape
-        self._index = index_exp[index]
+        self._index = numpy.index_exp[index]
 
-        # Set the node label
-        self._label = '{0}{1}'.format(variable, index_str(index))
-
-        # Close the NetCDF file
-        ncfile.close()
+        # Call the base class initializer
+        super(ReadDataNode, self).__init__('{0}{1}'.format(variable, index_str(index)))
 
     @property
     def variable(self):
@@ -245,3 +278,110 @@ class ReaderNode(DataNode):
             data = DataArray(ncvar[rindex], units=units, dimensions=rdimensions)
 
         return data
+
+
+#===================================================================================================
+# ValidateDataNode
+#===================================================================================================
+class ValidateDataNode(DataNode):
+    """
+    The basic class to validate data
+    """
+    
+    def __init__(self, label, dnode, minimum=None, maximum=None,
+                 min_mean_abs=None, max_mean_abs=None):
+        """
+        Initializer
+        
+        Parameters:
+            label: A label to give the DataNode
+            dnode (DataNode): DataNode that provides input into this DataNode
+            minimum: The minimum value the data should have, if valid
+            maximum: The maximum value the data should have, if valid
+            min_mean_abs: The minimum acceptable value of the mean of the 
+                absolute value of the data
+            max_mean_abs: The maximum acceptable value of the mean of the 
+                absolute value of the data
+        """
+        # Check DataNode type
+        if not isinstance(dnode, DataNode):
+            raise TypeError('ValidatorNode can only validate output from a DataNode')
+        
+        # Store min/max
+        self._min = minimum
+        self._max = maximum
+
+        # Stoe mean_abs min/max
+        self._min_mean_abs = min_mean_abs
+        self._max_mean_abs = max_mean_abs
+
+        # Call base class initializer
+        super(ValidateDataNode, self).__init__(label, dnode)
+
+    def __getitem__(self, index):
+        """
+        Compute and retrieve the data associated with this DataNode operation
+        """
+        data = self._inputs[0][index]
+        
+        if self._min is not None:
+            dmin = numpy.min(data)
+            if dmin < self._min:
+                warning(('Data from operator {!r} has minimum value '
+                         '{} but requires data greater than or equal to '
+                         '{}').format(self.name, dmin, self._min))
+
+        if self._max is not None:
+            dmax = numpy.max(data)
+            if dmax > self._max:
+                warning(('Data from operator {!r} has maximum value '
+                         '{} but requires data less than or equal to '
+                         '{}').format(self.name, dmax, self._max))
+
+        if self._min_mean_abs is not None or self._max_mean_abs is not None:
+            mean_abs = numpy.mean(numpy.abs(data))
+
+        if self._min_mean_abs is not None:
+            if mean_abs < self._min_mean_abs:
+                warning(('Data from operator {!r} has minimum mean_abs value '
+                         '{} but requires data greater than or equal to '
+                         '{}').format(self.name, mean_abs, self._min_mean_abs))
+
+        if self._max_mean_abs is not None:
+            if mean_abs > self._max:
+                warning(('Data from operator {!r} has maximum mean_abs value '
+                         '{} but requires data less than or equal to '
+                         '{}').format(self.name, mean_abs, self._max_mean_abs))
+
+        return data
+
+
+#===================================================================================================
+# SetDataNode
+#===================================================================================================
+class SetDataNode(DataNode):
+    """
+    The basic class to set data in memory
+    """
+    
+    def __init__(self, label, data, units=None, dimensions=None):
+        """
+        Initializer
+        
+        Parameters:
+            label: A label to give the DataNode
+            data (array): Data to store in this DataNode
+            units: Units to associate with the data
+            dimensions: Dimensions to associate with the data 
+        """
+        # Store data
+        self._data = DataArray(data, units=units, dimensions=dimensions)
+
+        # Call base class initializer
+        super(SetDataNode, self).__init__(label)
+
+    def __getitem__(self, index):
+        """
+        Compute and retrieve the data associated with this DataNode operation
+        """
+        return self._data[index]
