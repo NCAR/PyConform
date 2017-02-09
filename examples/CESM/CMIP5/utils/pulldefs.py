0bd99d0f84799de2a065bac84fc480dd6e51d0f0
Kevin Paul <kpaul@ucar.edu>
2017-02-09 09:56:01 -0700

    Starting script to pull out a "definitions" file from a specfile

---
+++ examples/CESM/CMIP5/utils/pulldefs.py
@@ -0,0 +1,8 @@
+"""
+mkDefFile
+
+Command-Line Utility to create a starting definitions file from a specfile
+
+Copyright 2017, University Corporation for Atmospheric Research
+LICENSE: See the LICENSE.rst file for details
+"""
