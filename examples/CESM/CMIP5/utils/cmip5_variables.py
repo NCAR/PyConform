67c6d62fe143e96a56ab781e03b8e8d5bfb2cd1a
Kevin Paul <kpaul@ucar.edu>
2017-02-16 14:50:45 -0700

    Removing limits on input

--- examples/CESM/CMIP5/utils/cmip5_variables.py
+++ examples/CESM/CMIP5/utils/cmip5_variables.py
@@ -64,7 +64,7 @@ def main(argv=None):
     
     # Variables by attributes
     vatts = {}
-    for ncvar in ncvars[:5]:
+    for ncvar in ncvars:
         xfrte = pjoin(*ncvar[:5])
         print '{}:'.format(xfrte)
         vars = ncvar[5:]
