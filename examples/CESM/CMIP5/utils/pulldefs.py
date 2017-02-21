b65444c1eda6a2c9fd466a4e879d821664317471
Kevin Paul <kpaul@ucar.edu>
2017-02-21 14:38:07 -0700

    Renaming and adding functionality to pull script

--- examples/CESM/CMIP5/utils/pulldefs.py
+++
@@ -1,8 +0,0 @@
-"""
-pulldefs
-
-Command-Line Utility to pull out definitions from a specfile
-
-Copyright 2017, University Corporation for Atmospheric Research
-LICENSE: See the LICENSE.rst file for details
-"""
