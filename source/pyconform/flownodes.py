0fab44054e237e4f322f990bd799c4bd2a46a41c
Kevin Paul <kpaul@ucar.edu>
2017-06-01 16:21:34 -0600

    More debugging output

--- source/pyconform/flownodes.py
+++ source/pyconform/flownodes.py
@@ -797,7 +797,7 @@ class WriteNode(FlowNode):
         # Open the file and write the header information
         self.open(history=history)
 
-        print self._idims
+        print self.label, self._idims
         
         # Loop over all variable nodes and write data
         for vnode in self.inputs:
