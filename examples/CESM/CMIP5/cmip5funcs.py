6088055c110afee52bd74038e655f83f6f5999c9
Kevin Paul <kpaul@ucar.edu>
2017-06-13 12:24:51 -0600

    New way of computing bounds from interface variables

--- examples/CESM/CMIP5/cmip5funcs.py
+++ examples/CESM/CMIP5/cmip5funcs.py
@@ -4,7 +4,7 @@ from Ngl import vinth2p
 from pyconform.physarray import PhysArray, UnitsError, DimensionsError
 from pyconform.functions import Function
 from cf_units import Unit
-from numpy import diff
+from numpy import diff, empty
         
         
 #===================================================================================================
@@ -13,7 +13,7 @@ from numpy import diff
 class BoundsFunction(Function):
     key = 'bounds'
     
-    def __call__(self, data, bdim='bnds', location=1, endpoints=1):
+    def __call__(self, data, bdim='bnds', location=1, endpoints=1, idata=None):
         if not isinstance(data, PhysArray):
             raise TypeError('bounds: data must be a PhysArray')
         if not isinstance(bdim, basestring):
@@ -23,29 +23,68 @@ class BoundsFunction(Function):
         if len(data.dimensions) != 1:
             raise DimensionsError('bounds: data can only be 1D')
         mod_end = bool(endpoints)
-
-        bnds = PhysArray([1, 1], dimensions=(bdim,))
-        new_data = PhysArray(data * bnds, name='bounds({})'.format(data.name))
-        if len(new_data) == 0:
+        
+        self.add_sumlike_dimensions(data.dimensions[0])
+        
+        if idata is None:
+            bnds = PhysArray([1, 1], dimensions=(bdim,))
+            new_data = PhysArray(data * bnds, name='bounds({})'.format(data.name))
+            if len(new_data) == 0:
+                return new_data
+            dx = diff(data.data)
+            if location == 0:
+                new_data[:-1,1] = data.data[:-1] + dx
+                if mod_end:
+                    new_data[-1,1] = data.data[-1] + dx[-1]
+            elif location == 1:
+                hdx = 0.5 * dx
+                new_data[1:,0] = data.data[1:] - hdx
+                new_data[:-1,1] = data.data[:-1] + hdx
+                if mod_end:
+                    new_data[0,0] = data.data[0] - hdx[0]
+                    new_data[-1,1] = data.data[-1] + hdx[-1]
+            elif location == 2:
+                new_data[1:,0] = data.data[1:] - dx
+                if mod_end:
+                    new_data[0,0] = data.data[0] - dx[0]
             return new_data
-        dx = diff(data.data)
-        if location == 0:
-            new_data[:-1,1] = data.data[:-1] + dx
-            if mod_end:
-                new_data[-1,1] = data.data[-1] + dx[-1]
-        elif location == 1:
-            hdx = 0.5 * dx
-            new_data[1:,0] = data.data[1:] - hdx
-            new_data[:-1,1] = data.data[:-1] + hdx
-            if mod_end:
-                new_data[0,0] = data.data[0] - hdx[0]
-                new_data[-1,1] = data.data[-1] + hdx[-1]
-        elif location == 2:
-            new_data[1:,0] = data.data[1:] - dx
-            if mod_end:
-                new_data[0,0] = data.data[0] - dx[0]
-        return new_data
+        
+        else:
+            if not isinstance(idata, PhysArray):
+                raise TypeError('bounds: interface-data must be a PhysArray')
+            if len(idata.dimensions) != 1:
+                raise DimensionsError('bounds: interface-data can only be 1D')
+            self.add_sumlike_dimensions(idata.dimensions[0])
+            
+            ifc_len = len(data) + 1
+            ifc_data = empty(ifc_len, dtype=data.dtype)
+            if len(idata) == ifc_len:
+                ifc_data[:] = idata.data[:]
+            elif len(idata) == ifc_len - 2:
+                ifc_data[1:-1] = idata.data[:]
+                if location == 0:
+                    ifc_data[0] = data.data[0]
+                    ifc_data[-1] = 2*data.data[-1] - data.data[-2]
+                elif location == 1:
+                    ifc_data[0] = 2*data.data[0] - idata.data[0]
+                    ifc_data[-1] = 2*data.data[-1] - idata.data[-1]
+                else:
+                    ifc_data[0] = 2*data.data[0] - data.data[1]
+                    ifc_data[-1] = data.data[-1]
+            else:
+                raise ValueError('bounds: interface-data length is {} but should be {} or '
+                                 '{}'.format(len(idata), ifc_len, ifc_len-2))
 
+            bnds_data = empty((len(data), 2), dtype=data.dtype)
+            bnds_data[:,0] = ifc_data[:-1]
+            bnds_data[:,1] = ifc_data[1:]
+            
+            new_dims = data.dimensions + (bdim,)
+            new_name = 'bounds({})'.format(data.name)
+            new_units = data.units
+            
+            return PhysArray(bnds_data, mask=data.mask, name=new_name, units=new_units, dimensions=new_dims)
+            
 
 #===================================================================================================
 # VertInterpFunction
@@ -54,9 +93,7 @@ class VertInterpFunction(Function):
     key = 'vinth2p'
 
     def __call__(self, datai, hbcofa, hbcofb, plevo, psfc, p0, intyp=1, ixtrp=0):
-        if (not isinstance(datai, PhysArray) or not isinstance(hbcofa, PhysArray) or
-            not isinstance(hbcofb, PhysArray) or not isinstance(plevo, PhysArray) or
-            not isinstance(psfc, PhysArray) or not isinstance(p0, PhysArray)):
+        if not all(isinstance(obj, PhysArray) for obj in (datai, hbcofa, hbcofb, plevo, psfc, p0)):
             raise TypeError('vinth2p: arrays must be PhysArrays')
         
         if len(datai.dimensions) != 3 and len(datai.dimensions) != 4:
