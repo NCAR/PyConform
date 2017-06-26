8b241ccf1f89c5ce101e3c8baa8d4f98cec15cdd
Kevin Paul <kpaul@ucar.edu>
2017-06-26 16:16:44 -0600

    Debugging

--- examples/CESM/CMIP5/utilities/ncdiff.py
+++ examples/CESM/CMIP5/utilities/ncdiff.py
@@ -67,13 +67,10 @@ def _cmp(a1,a2,rtol=1e-5,atol=1e-8):
         return not allclose(a1, a2, rtol=rtol, atol=atol)
     else:
         try:
-            print 'allclose({}, {}):'.format(a1, a2)
             res = allclose(a1, a2, rtol=rtol, atol=atol)
         except:
-            print '{} != {}:'.format(a1, a2)
             res = a1 != a2
-        print '       {}'.format(res)
-        return res
+        return not res
 
 
 #=======================================================================================================================
