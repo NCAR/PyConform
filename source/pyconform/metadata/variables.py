cea3c3c56e627b8e5338ba5e3bbf9c57a1d4968b
Kevin Paul <kpaul@ucar.edu>
2018-01-09 10:02:47 -0700

    Setting variable properties

--- source/pyconform/metadata/variables.py
+++ source/pyconform/metadata/variables.py
@@ -6,7 +6,7 @@ LICENSE: See the LICENSE.rst file for details
 """
 
 from cf_units import Unit
-
+from numpy import ndarray
 from namedobjects import NamedObject
 
 
@@ -15,11 +15,45 @@ class Variable(NamedObject):
     Metadata describing a NetCDF variable
     """
 
-    def __init__(self, name):
+    _NETCDF_TYPES_ = ('byte', 'ubyte', 'char',
+                      'short', 'ushort',
+                      'int', 'uint',
+                      'int64', 'uint64',
+                      'float', 'real', 'double')
+
+    def __init__(self, name, definition=None, datatype=None):
         super(Variable, self).__init__(name)
-        self.definition = None
-        self.datatype = None
-        self.attributes = {}
+        self.__definition = self.__validate_definition_type(definition)
+        self.__datatype = self.__validate_datatype(datatype)
+        self.__attributes = {}
+
+    def __validate_definition_type(self, definition):
+        if definition is None:
+            return None
+        if not isinstance(definition, (basestring, ndarray)):
+            msg = 'Variable {} must have a string or array definition'
+            raise TypeError(msg.format(self.name))
+        return definition
+
+    def __validate_datatype(self, datatype):
+        if datatype is None:
+            return None
+        if datatype not in Variable._NETCDF_TYPES_:
+            msg = 'Variable {} has invalid datatype {!r}'
+            raise ValueError(msg.format(self.name, datatype))
+        return datatype
+
+    @property
+    def definition(self):
+        return self.__definition
+
+    @property
+    def datatype(self):
+        return self.__datatype
+
+    @property
+    def attributes(self):
+        return self.__attributes
 
     @property
     def units(self):
--- source/test/metadata/variablesTests.py
+++ source/test/metadata/variablesTests.py
@@ -8,6 +8,8 @@ LICENSE: See the LICENSE.rst file for details
 import unittest
 
 from cf_units import Unit
+from numpy import array
+from numpy.testing import assert_array_equal
 from pyconform.metadata import Variable
 
 
@@ -22,11 +24,44 @@ class VariableTests(unittest.TestCase):
     def test_default_definition_is_none(self):
         self.assertEqual(self.v.definition, None)
 
+    def test_setting_definition_raises_attribute_error(self):
+        with self.assertRaises(AttributeError):
+            self.v.definition = 'f(x)'
+
+    def test_setting_definition_to_str_in_constructor(self):
+        v = Variable('v', definition='f(x)')
+        self.assertEqual(v.definition, 'f(x)')
+
+    def test_setting_definition_to_array_in_constructor(self):
+        defn = array([1, 2, 3], dtype='f')
+        v = Variable('v', definition=defn)
+        assert_array_equal(v.definition, defn)
+
+    def test_setting_definition_to_invalid_type_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            Variable('v', definition=4)
+
     def test_default_datatype_is_none(self):
         self.assertEqual(self.v.datatype, None)
 
+    def test_setting_datatype_raises_attribute_error(self):
+        with self.assertRaises(AttributeError):
+            self.v.datatype = 'integer'
+
+    def test_setting_datatype_in_constructor(self):
+        v = Variable('v', datatype='int')
+        self.assertEqual(v.datatype, 'int')
+
+    def test_setting_datatype_to_invalid_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            Variable('v', datatype='integer')
+
     def test_default_attributes_is_empty_dict(self):
         self.assertEqual(self.v.attributes, {})
+
+    def test_setting_attributes_raises_attribute_error(self):
+        with self.assertRaises(AttributeError):
+            self.v.attributes = 4
 
     def test_default_units_is_none(self):
         self.assertEqual(self.v.units, None)
