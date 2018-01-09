f2ad2ed135700c794d0b70059c64aa65759ed29a
Kevin Paul <kpaul@ucar.edu>
2018-01-09 09:36:59 -0700

    Making attributes immutable as properties

--- source/pyconform/metadata/dimensions.py
+++ source/pyconform/metadata/dimensions.py
@@ -13,7 +13,35 @@ class Dimension(NamedObject):
     Metadata describing a NetCDF dimension
     """
 
-    def __init__(self, name):
+    def __init__(self, name, size=None, is_unlimited=False):
         super(Dimension, self).__init__(name)
-        self.size = None
-        self.is_unlimited = False
+        self.__size = self.__validate_size_type(size)
+        self.__is_unlimited = self.__validate_is_unlimited_type(is_unlimited)
+
+    def __validate_size_type(self, size):
+        if size is None:
+            return None
+        if not isinstance(size, int):
+            msg = 'Dimension {} must have integer size'
+            raise TypeError(msg.format(self.name))
+        return self.__validate_size_value(size)
+
+    def __validate_size_value(self, size):
+        if size < 1:
+            msg = 'Dimension {} must have positive size'
+            raise ValueError(msg.format(self.name))
+        return size
+
+    def __validate_is_unlimited_type(self, is_unlimited):
+        if not isinstance(is_unlimited, bool):
+            msg = 'Dimension {} must have bool is_unlimited'
+            raise TypeError(msg.format(self.name))
+        return is_unlimited
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def is_unlimited(self):
+        return self.__is_unlimited
--- source/test/metadata/dimensionsTests.py
+++ source/test/metadata/dimensionsTests.py
@@ -21,8 +21,36 @@ class DimensionTests(unittest.TestCase):
     def test_default_size_is_none(self):
         self.assertEqual(self.d.size, None)
 
+    def test_setting_size_property_raises_attribute_error(self):
+        with self.assertRaises(AttributeError):
+            self.d.size = 5
+
+    def test_set_size_to_int_in_constructor(self):
+        d = Dimension('x', size=4)
+        self.assertEqual(d.size, 4)
+
+    def test_set_size_to_non_int_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            Dimension('x', size='4')
+
+    def test_set_size_to_non_positive_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            Dimension('x', size=-2)
+
     def test_default_is_unlimited_is_false(self):
         self.assertEqual(self.d.is_unlimited, False)
+
+    def test_setting_is_unlimited_property_raises_attribute_error(self):
+        with self.assertRaises(AttributeError):
+            self.d.is_unlimited = True
+
+    def test_set_is_unlimited_to_bool_in_constructor(self):
+        d = Dimension('x', is_unlimited=True)
+        self.assertEqual(d.is_unlimited, True)
+
+    def test_set_is_unlimited_to_non_bool_raises_type_error(self):
+        with self.assertRaises(TypeError):
+            Dimension('x', is_unlimited='false')
 
 
 if __name__ == '__main__':
