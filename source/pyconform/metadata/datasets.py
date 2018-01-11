9959c57809686a97588af870635dfe27e589234a
Kevin Paul <kpaul@ucar.edu>
2018-01-11 14:05:43 -0700

    Changing to construction via constructor (not add method)

--- source/pyconform/metadata/datasets.py
+++ source/pyconform/metadata/datasets.py
@@ -14,10 +14,41 @@ class Dataset(object):
     Metadata describing an entire NetCDF dataset
     """
 
-    def __init__(self):
+    def __init__(self, files=()):
         self.__dimensions = OrderedDict()
         self.__variables = OrderedDict()
         self.__files = OrderedDict()
+        self.__validate_files(files)
+
+    def __validate_files(self, files):
+        if not isinstance(files, (tuple, list)) or not all(isinstance(f, File) for f in files):
+            raise TypeError('Dataset files must be a tuple/list of Files')
+        for f in files:
+            self.__add_file(f)
+
+    def __add_file(self, f):
+        if f.name in self.__files and f is not self.__files[f.name]:
+            msg = 'A File with name {} is already contained in Dataset'
+            raise ValueError(msg.format(f.name))
+        self.__files[f.name] = f
+        for v in f.variables:
+            self.__add_variable(v)
+
+    def __add_variable(self, v):
+        if v.name in self.__variables and v is not self.__variables[v.name]:
+            msg = 'A Variable with name {} is already contained in Dataset'
+            raise ValueError(msg.format(v.name))
+        self.__variables[v.name] = v
+        if v.dimensions is None:
+            return
+        for d in v.dimensions:
+            self.__add_dimension(d)
+
+    def __add_dimension(self, d):
+        if d.name in self.__dimensions and d is not self.__dimensions[d.name]:
+            msg = 'A Dimension with name {} is already contained in Dataset'
+            raise ValueError(msg.format(d.name))
+        self.__dimensions[d.name] = d
 
     @property
     def dimensions(self):
@@ -40,38 +71,3 @@ class Dataset(object):
             return obj.name in self.__files and obj is self.__files[obj.name]
         else:
             return False
-
-    def add(self, obj):
-        if isinstance(obj, Dimension):
-            self.__add_dimension(obj)
-        elif isinstance(obj, Variable):
-            self.__add_variable(obj)
-        elif isinstance(obj, File):
-            self.__add_file(obj)
-        else:
-            msg = 'Dataset cannot add object of type {}'
-            raise TypeError(msg.format(type(obj)))
-
-    def __add_dimension(self, d):
-        if d.name in self.__dimensions and d is not self.__dimensions[d.name]:
-            msg = 'A Dimension with name {} is already contained in Dataset'
-            raise ValueError(msg.format(d.name))
-        self.__dimensions[d.name] = d
-
-    def __add_variable(self, v):
-        if v.name in self.__variables and v is not self.__variables[v.name]:
-            msg = 'A Variable with name {} is already contained in Dataset'
-            raise ValueError(msg.format(v.name))
-        self.__variables[v.name] = v
-        if v.dimensions is None:
-            return
-        for d in v.dimensions:
-            self.__add_dimension(d)
-
-    def __add_file(self, f):
-        if f.name in self.__files and f is not self.__files[f.name]:
-            msg = 'A File with name {} is already contained in Dataset'
-            raise ValueError(msg.format(f.name))
-        self.__files[f.name] = f
-        for v in f.variables:
-            self.__add_variable(v)
--- source/test/metadata/datasetsTests.py
+++ source/test/metadata/datasetsTests.py
@@ -25,21 +25,9 @@ class DatasetTests(unittest.TestCase):
         with self.assertRaises(AttributeError):
             self.ds.dimensions = 3
 
-    def test_add_dimension(self):
-        d = Dimension('x')
-        self.ds.add(d)
-        self.assertIn(d, self.ds)
-
     def test_add_wrong_type_raises_type_error(self):
         with self.assertRaises(TypeError):
-            self.ds.add('x')
-
-    def test_add_different_dimensions_with_same_name_raises_value_error(self):
-        d1 = Dimension('x')
-        d2 = Dimension('x')
-        self.ds.add(d1)
-        with self.assertRaises(ValueError):
-            self.ds.add(d2)
+            Dataset(files=('x', 'y'))
 
     def test_default_variables_is_empty_tuple(self):
         self.assertEqual(self.ds.variables, [])
@@ -47,24 +35,6 @@ class DatasetTests(unittest.TestCase):
     def test_setting_variables_raises_attribute_error(self):
         with self.assertRaises(AttributeError):
             self.ds.variables = 3
-
-    def test_add_variable(self):
-        v = Variable('v')
-        self.ds.add(v)
-        self.assertIn(v, self.ds)
-
-    def test_add_variable_also_adds_dimensions(self):
-        d = Dimension('x')
-        v = Variable('v', dimensions=(d,))
-        self.ds.add(v)
-        self.assertIn(d, self.ds)
-
-    def test_add_different_variables_with_same_name_raises_value_error(self):
-        v1 = Variable('v')
-        v2 = Variable('v')
-        self.ds.add(v1)
-        with self.assertRaises(ValueError):
-            self.ds.add(v2)
 
     def test_default_files_is_empty_tuple(self):
         self.assertEqual(self.ds.files, [])
@@ -75,23 +45,22 @@ class DatasetTests(unittest.TestCase):
 
     def test_add_file(self):
         f = File('test.nc')
-        self.ds.add(f)
-        self.assertIn(f, self.ds)
+        ds = Dataset(files=(f,))
+        self.assertIn(f, ds)
 
     def test_add_different_files_with_same_name_raises_value_error(self):
         f1 = File('test.nc')
         f2 = File('test.nc')
-        self.ds.add(f1)
         with self.assertRaises(ValueError):
-            self.ds.add(f2)
+            Dataset(files=(f1, f2))
 
     def test_add_file_also_adds_variables_and_dimensions(self):
         d = Dimension('x')
         v = Variable('v', dimensions=(d,))
         f = File('test.nc', variables=(v,))
-        self.ds.add(f)
-        self.assertIn(v, self.ds)
-        self.assertIn(d, self.ds)
+        ds = Dataset(files=(f,))
+        self.assertIn(v, ds)
+        self.assertIn(d, ds)
 
 
 if __name__ == '__main__':
