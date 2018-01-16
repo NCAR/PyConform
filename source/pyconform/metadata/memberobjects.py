54a8f12112828fd267fbd54655a23b615fa69471
Kevin Paul <kpaul@ucar.edu>
2018-01-16 11:58:57 -0700

    Renaming and debug

--- source/pyconform/metadata/memberobjects.py
+++ source/pyconform/metadata/memberobjects.py
@@ -16,7 +16,7 @@ class MemberObject(NamedObject):
     def __init__(self, name, **kwds):
         super(MemberObject, self).__init__(name)
         if 'dataset' not in kwds:
-            clsname = self.__class__.__name__,
+            clsname = self.__class__.__name__
             msg = '{} {!r} needs a dataset for construction'
             raise AttributeError(msg.format(clsname, self.name))
         self.__dataset = kwds['dataset']
