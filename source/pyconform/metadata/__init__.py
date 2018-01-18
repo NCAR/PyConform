eb7606022fccd62cfaf8d936cbc9ad8dc692f97d
Kevin Paul <kpaul@ucar.edu>
2018-01-18 09:08:04 -0700

    Redirecting imports

--- source/pyconform/metadata/__init__.py
+++ source/pyconform/metadata/__init__.py
@@ -5,7 +5,7 @@ Copyright 2017-2018, University Corporation for Atmospheric Research
 LICENSE: See the LICENSE.rst file for details
 """
 
-from pyconform.metadata.dimensions.dimensions import Dimension
+from dimensions import Dimension
 from variables import Variable
 from files import File
 from inputdatasets import InputDataset
--- source/pyconform/metadata/dimensions/__init__.py
+++ source/pyconform/metadata/dimensions/__init__.py
@@ -4,3 +4,5 @@ Dimension Metadata Classes
 Copyright 2017-2018, University Corporation for Atmospheric Research
 LICENSE: See the LICENSE.rst file for details
 """
+
+from dimensions import Dimension
