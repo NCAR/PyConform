bb03e5c47f80c31bfb20b866ad2e1ed38053058a
Kevin Paul <kpaul@ucar.edu>
2018-02-05 08:01:21 -0700

    Starting physarray tests

---
+++ source/test/physarrays/physarraysTests.py
@@ -0,0 +1,18 @@
+"""
+Unit Tests for PhysArray Class and Functions
+
+Copyright 2017-2018, University Corporation for Atmospheric Research
+LICENSE: See the LICENSE.rst file for details
+"""
+
+import unittest
+
+
+class PhysArrayTests(unittest.TestCase):
+
+    def test_create(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
