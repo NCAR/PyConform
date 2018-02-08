5eaaa3f8ba54578b5a803894b973b5c0928723db
Kevin Paul <kpaul@ucar.edu>
2018-02-08 16:30:02 -0700

    Creating packages for rewritten parser

---
+++ source/pyconform/defparsing/__init__.py
@@ -0,0 +1,6 @@
+"""
+Classing and Functions for Definition Parsing
+
+Copyright 2017-2018, University Corporation for Atmospheric Research
+LICENSE: See the LICENSE.rst file for details
+"""
---
+++ source/test/defparsing/__init__.py
@@ -0,0 +1,6 @@
+"""
+Unit Tests for the Definition Parser
+
+Copyright 2017-2018, University Corporation for Atmospheric Research
+LICENSE: See the LICENSE.rst file for details
+"""
