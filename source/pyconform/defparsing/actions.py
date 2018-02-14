c38478d1d1fd299a45570e9295bbb3bd13bc94b9
Kevin Paul <kpaul@ucar.edu>
2018-02-14 16:08:35 -0700

    Adding parse action functions

---
+++ source/pyconform/defparsing/actions.py
@@ -0,0 +1,28 @@
+"""
+Parse Actions
+
+Copyright 2017-2018, University Corporation for Atmospheric Research
+LICENSE: See the LICENSE.rst file for details
+"""
+
+from collections import namedtuple
+
+VariableType = namedtuple('variable', ['name', 'indices'])
+VariableType.__new__.__defaults__ = (None, None)
+
+FunctionType = namedtuple('function', ['name', 'arguments', 'keywords'])
+FunctionType.__new__.__defaults__ = (None, tuple(), {})
+
+
+def integer_action(tokens):
+    return int(tokens[0])
+
+
+def float_action(tokens):
+    return float(tokens[0])
+
+
+def variable_action(tokens):
+    name = tokens[0]
+    indices = None if len(tokens) == 1 else tokens[1:]
+    return VariableType(name, indices)
---
+++ source/test/defparsing/actionsTests.py
@@ -0,0 +1,44 @@
+"""
+Unit Tests for Parse Actions
+
+Copyright 2017-2018, University Corporation for Atmospheric Research
+LICENSE: See the LICENSE.rst file for details
+"""
+
+from pyconform.defparsing import actions
+
+import unittest
+
+
+class Tests(unittest.TestCase):
+
+    def test_integer_action(self):
+        result = actions.integer_action(['1'])
+        self.assertEqual(result, 1)
+
+    def test_float_action_on_int(self):
+        result = actions.float_action(['1.0'])
+        self.assertEqual(result, 1.0)
+
+    def test_float_action_on_exp(self):
+        result = actions.float_action(['1e5'])
+        self.assertEqual(result, 1e5)
+
+    def test_variable_action_on_solo_symbol(self):
+        result = actions.variable_action(['x'])
+        self.assertEqual(result.name, 'x')
+        self.assertEqual(result.indices, None)
+
+    def test_variable_action_with_1D_slice(self):
+        result = actions.variable_action(['x', slice(1, 5)])
+        self.assertEqual(result.name, 'x')
+        self.assertEqual(result.indices, [slice(1, 5)])
+
+    def test_variable_action_with_2D_slice(self):
+        result = actions.variable_action(['x', slice(1, 5), slice(None, 3)])
+        self.assertEqual(result.name, 'x')
+        self.assertEqual(result.indices, [slice(1, 5), slice(None, 3)])
+
+
+if __name__ == "__main__":
+    unittest.main()
