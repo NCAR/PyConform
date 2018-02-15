14dc2e07bad998c4c686ce9e37125fdecbb3e6d0
Kevin Paul <kpaul@ucar.edu>
2018-02-15 16:54:45 -0700

    Testing combinations of functions, lists, and variables

--- source/test/defparsing/recursionsTests.py
+++ source/test/defparsing/recursionsTests.py
@@ -7,6 +7,7 @@ LICENSE: See the LICENSE.rst file for details
 
 from pyconform.defparsing import recursions as rcr
 from pyconform.defparsing import actions as act
+from collections import OrderedDict
 
 import unittest
 
@@ -35,13 +36,26 @@ class Tests(unittest.TestCase):
 
     def test_function_kwd_arguments(self):
         token = rcr.expression.parseString('f(a=3,b=4)')[0]
-        self.assertEqual(token, act.FunctionType(
-            'f', tuple(), {'a': 3, 'b': 4}))
+        fobj = act.FunctionType('f', tuple(), {'a': 3, 'b': 4})
+        self.assertEqual(token, fobj)
 
     def test_function_pos_and_kwd_arguments(self):
         token = rcr.expression.parseString('f(1,2,a=3,b=4)')[0]
-        self.assertEqual(token, act.FunctionType(
-            'f', (1, 2), {'a': 3, 'b': 4}))
+        fobj = act.FunctionType('f', (1, 2), {'a': 3, 'b': 4})
+        self.assertEqual(token, fobj)
+
+    def test_function_with_variable_argument(self):
+        token = rcr.expression.parseString('f(1,x[2:4, 6],a=3,b=4)')[0]
+        vobj = act.VariableType('x', [slice(2, 4), 6])
+        fobj = act.FunctionType('f', (1, vobj), {'a': 3, 'b': 4})
+        self.assertEqual(token, fobj)
+
+    def test_function_with_list_and_variable_argument(self):
+        token = rcr.expression.parseString('f(1,x[2:4, 6],a=[4,5],b=6)')[0]
+        vobj = act.VariableType('x', [slice(2, 4), 6])
+        kwds = OrderedDict([('a', [4, 5]), ('b', 6)])
+        fobj = act.FunctionType('f', (1, vobj), kwds)
+        self.assertEqual(token, fobj)
 
 
 if __name__ == "__main__":
