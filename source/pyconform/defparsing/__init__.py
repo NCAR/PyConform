532e74df0fcafaec726d07c9f27ee0f0ba20d9c1
Kevin Paul <kpaul@ucar.edu>
2018-02-19 16:15:05 -0700

    Consolidating code and simplifying.

--- source/pyconform/defparsing/__init__.py
+++ source/pyconform/defparsing/__init__.py
@@ -4,3 +4,5 @@ Classing and Functions for Definition Parsing
 Copyright 2017-2018, University Corporation for Atmospheric Research
 LICENSE: See the LICENSE.rst file for details
 """
+
+from expressions import parse_definition, KwdType, FuncType, VarType, OpType
