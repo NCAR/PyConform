4dcea79e350ddd7bb278babb5e6e080eca2ee489
Kevin Paul <kpaul@ucar.edu>
2018-02-20 13:46:25 -0700

    Renaming and updating references

--- source/test/streams/expressionsTests.py
+++ source/test/streams/expressionsTests.py
@@ -5,7 +5,7 @@ Copyright 2017-2018, University Corporation for Atmospheric Research
 LICENSE: See the LICENSE.rst file for details
 """
 
-from pyconform.defparsing import expressions
+from pyconform.streams import expressions
 
 import unittest
 
