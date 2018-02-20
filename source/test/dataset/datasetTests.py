aebb7c684e07f9f22701b43c09193a7cd0601433
Kevin Paul <kpaul@ucar.edu>
2018-02-20 13:58:58 -0700

    Renaming to avoid collision with existing module name

--- source/test/dataset/datasetTests.py
+++ source/test/dataset/datasetTests.py
@@ -1,14 +1,15 @@
 """
-Unit Tests
+Unit Tests for Input Dataset Objects
 
 Copyright 2017-2018, University Corporation for Atmospheric Research
 LICENSE: See the LICENSE.rst file for details
 """
 
+from pyconform.dataset import Dataset
 import unittest
 
 
-class Test(unittest.TestCase):
+class DatasetTests(unittest.TestCase):
 
     def testName(self):
         pass
