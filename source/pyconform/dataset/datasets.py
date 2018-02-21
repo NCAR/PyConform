b49bf5bff93a8e7cbb6d4b61833812e2aa5a1391
Kevin Paul <kpaul@ucar.edu>
2018-02-20 17:15:30 -0700

    Adding methods to easily grab variables and convert to physarrays

--- source/pyconform/dataset/datasets.py
+++ source/pyconform/dataset/datasets.py
@@ -5,6 +5,9 @@ Copyright 2017-2018, University Corporation for Atmospheric Research
 LICENSE: See the LICENSE.rst file for details
 """
 
+from collections import OrderedDict
+from pyconform.physarray import PhysArray
+
 import xarray as xr
 
 
@@ -13,15 +16,23 @@ class Dataset(object):
     A container for Xarray dataset objects
     """
 
-    def __init__(self, filenames=[]):
-        self.__datasets = {}
+    def __init__(self, filenames=None):
+        ds = xr.open_mfdataset(filenames, autoclose=True, chunks={})
+        self.__dataset = ds
+        self.__datavars = OrderedDict((v, PhysArray(ds.data_vars[v]))
+                                      for v in ds.data_vars)
 
     def __contains__(self, name):
-        for ds in self.__datasets:
-            if name in self.__datasets[ds].data_vars:
-                return True
-        return False
+        return name in self.__dataset.data_vars or name in self.__dataset.coords
 
-    def add_file(self, filename):
-        self.__datasets[filename] = xr.open_dataset(
-            filename, autoclose=True, chunks={})
+    @property
+    def dims(self):
+        return self.__dataset.dims
+
+    def __getitem__(self, name):
+        if name in self.__dataset.data_vars:
+            return PhysArray(self.__dataset.data_vars[name])
+        elif name in self.__dataset.coords:
+            return PhysArray(self.__dataset.coords[name])
+        else:
+            raise KeyError('{!r} not found in Dataset'.format(name))
--- source/test/dataset/datasetsTests.py
+++ source/test/dataset/datasetsTests.py
@@ -6,48 +6,70 @@ LICENSE: See the LICENSE.rst file for details
 """
 
 from pyconform.dataset import Dataset
+from pyconform.physarray import PhysArray
 
 import unittest
 import netCDF4 as nc
 import numpy as np
+import os
 
 
 class DatasetTests(unittest.TestCase):
 
     def setUp(self):
-        self.ds = Dataset()
-        self.filename = 'test.nc'
-        self.dimensions = {'x': 100, 'y': 150, 't': 50}
-        self.variables = ['v1', 'v2', 'v3']
+        self.dimensions = {'x': 50, 'y': 40, 't': 30}
+        self.variables = ['v', 'u', 'w']
+        self.filenames = {v: '{}.nc'.format(v) for v in self.variables}
         self.vardims = ('x', 'y', 't')
-        self._write_file_()
+        self.cleanUp()
+        self.writeTimeSeries()
+        self.ds = Dataset(self.filenames.values())
 
-    def _write_file_(self):
-        f = nc.Dataset(self.filename, 'w')
-        for d in self.dimensions:
-            f.createDimension(d, None if d == 't' else self.dimensions[d])
-            f.createVariable(d, 'd', (d,))
-            if d == 't':
-                f.variables[d].setncattr('units', 'days since 1974-02-06')
-                f.variables[d].setncattr('calendar', 'noleap')
-            else:
-                f.variables[d].setncattr('units', 'g')
-        for v in self.variables:
+    def tearDown(self):
+        self.cleanUp()
+
+    def cleanUp(self):
+        for filename in self.filenames.values():
+            if os.path.exists(filename):
+                os.remove(filename)
+
+    def writeTimeSeries(self):
+        for n, v in enumerate(self.variables):
+            f = nc.Dataset(self.filenames[v], 'w')
+            for d in self.dimensions:
+                f.createDimension(d, None if d == 't' else self.dimensions[d])
+                f.createVariable(d, 'd', (d,))
+                if d == 't':
+                    f.variables[d].setncattr('units', 'days since 1974-02-06')
+                    f.variables[d].setncattr('calendar', 'noleap')
+                else:
+                    f.variables[d].setncattr('units', 'g')
             f.createVariable(v, 'f', self.vardims)
-        for d in self.dimensions:
-            f.variables[d][:] = np.arange(self.dimensions[d], dtype='d')
-        vshape = tuple(self.dimensions[d] for d in self.vardims)
-        for v in self.variables:
-            f.variables[v][:] = np.ones(vshape, dtype='f')
-        f.close()
+            for d in self.dimensions:
+                f.variables[d][:] = np.arange(self.dimensions[d], dtype='d')
+            vshape = tuple(self.dimensions[d] for d in self.vardims)
+            f.variables[v][:] = n * np.ones(vshape, dtype='f')
+            f.close()
 
     def test_create(self):
         self.assertIsInstance(self.ds, Dataset)
 
-    def test_add_file(self):
-        self.ds.add_file(self.filename)
+    def test_dims(self):
+        self.assertEqual(self.ds.dims, self.dimensions)
+
+    def test_contains(self):
+        for d in self.dimensions:
+            self.assertIn(d, self.ds)
         for v in self.variables:
             self.assertIn(v, self.ds)
+
+    def test_getitem_v_returns_physarray(self):
+        for v in self.variables:
+            va = self.ds[v]
+            self.assertIsInstance(va, PhysArray)
+        for d in self.dimensions:
+            da = self.ds[d]
+            self.assertIsInstance(da, PhysArray)
 
 
 if __name__ == '__main__':
