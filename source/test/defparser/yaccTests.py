3799a8b637546e035c4b3d18602ef4151e18a257
Kevin Paul <kpaul@ucar.edu>
2018-04-29 14:25:45 -0600

    More slice tests

--- source/test/defparser/yaccTests.py
+++ source/test/defparser/yaccTests.py
@@ -64,6 +64,18 @@ class YaccTests(unittest.TestCase):
         p = yacc_parse('x[2:-3:4, 7]')
         self.assertEqual(p, yacc.VarType('x', [slice(2, -3, 4), 7]))
 
+    def test_variable_slice_none_1(self):
+        p = yacc_parse('x[:-3:4]')
+        self.assertEqual(p, yacc.VarType('x', [slice(None, -3, 4)]))
+
+    def test_variable_slice_none_2(self):
+        p = yacc_parse('x[1::4]')
+        self.assertEqual(p, yacc.VarType('x', [slice(1, None, 4)]))
+
+    def test_variable_slice_none_3(self):
+        p = yacc_parse('x[1:4]')
+        self.assertEqual(p, yacc.VarType('x', [slice(1, 4)]))
+
 
 if __name__ == '__main__':
     unittest.main()
