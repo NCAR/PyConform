e48d3778b5ebed8fe42fa9ef2152e1e94205c5a8
Kevin Paul <kpaul@ucar.edu>
2018-06-11 16:30:03 -0600

    Starting work on Variable filters to match output

---
+++ source/pyconform/filters/variablefilter.py
@@ -0,0 +1,15 @@
+"""
+VariableFilter Class
+
+Copyright 2017-2018, University Corporation for Atmospheric Research
+LICENSE: See the LICENSE.rst file for details
+"""
+
+
+class VariableFilter(object):
+    """
+    Filter an input stream to match given output parameters
+    """
+
+    def __init__(self):
+        pass
