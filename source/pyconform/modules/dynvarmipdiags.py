62b32f15faa59776cff646be53b84528326f9b07
Kevin Paul <kpaul@ucar.edu>
2018-09-27 12:36:01 -0600

    Generalizing and fixing bugs

--- source/pyconform/modules/dynvarmipdiags.py
+++ source/pyconform/modules/dynvarmipdiags.py
@@ -18,125 +18,112 @@ g0 = 9.80665
 
 def _init_wtem(time, levi, lat, wzm, vthzm, thzm):
     ntime = len(time)
-    nlevi = len(levi)
-    nlat = len(lat)
 
     latrad = (lat / 180.0) * numpy.pi
     coslat = numpy.cos(latrad)
     levi100 = 100.0 * levi
 
-    _wzm = -1.0 * numpy.einsum('ijk,j->ijk', wzm, levi100) / H
+    _wzm = -1.0 * numpy.einsum('ij...,j->ij...', wzm, levi100) / H
 
-    wtem = numpy.zeros((ntime, nlevi, nlat), dtype='d')
+    wtem = numpy.zeros(wzm.shape, dtype='d')
     for itime in range(ntime):
-        dthdp = deriv(levi100, thzm[itime, :, :])
-        psieddy = vthzm[itime, :, :] / dthdp
+        dthdp = deriv(levi100, thzm[itime, ...])
+        psieddy = vthzm[itime, ...] / dthdp
         dpsidy = deriv(latrad, psieddy * coslat, axis=1) / (a * coslat)
-        wtem[itime, :, :] = _wzm[itime, :, :] + dpsidy
+        wtem[itime, ...] = _wzm[itime, ...] + dpsidy
     return wtem
 
 
 def wtem(time, levi, lat, wzm, vthzm, thzm):
     levi100 = 100.0 * levi
     wtem = _init_wtem(time, levi, lat, wzm, vthzm, thzm)
-    wtem = -1.0 * numpy.einsum('ijk,j->ijk', wtem, H / levi100)
+    wtem = -1.0 * numpy.einsum('ij...,j->ij...', wtem, H / levi100)
     return wtem
 
 
 def vtem(time, levi, lat, vzm, vthzm, thzm):
     ntime = len(time)
-    nlevi = len(levi)
-    nlat = len(lat)
 
     levi100 = 100.0 * levi
 
-    vtem = numpy.zeros((ntime, nlevi, nlat), dtype='d')
+    vtem = numpy.zeros(vzm.shape, dtype='d')
     for itime in range(ntime):
-        dthdp = deriv(levi100, thzm[itime, :, :])
-        psieddy = vthzm[itime, :, :] / dthdp
+        dthdp = deriv(levi100, thzm[itime, ...])
+        psieddy = vthzm[itime, ...] / dthdp
         dpsidp = deriv(levi100, psieddy)
-        vtem[itime, :, :] = vzm[itime, :, :] - dpsidp
+        vtem[itime, ...] = vzm[itime, ...] - dpsidp
     return vtem
 
 
 def utendvtem(time, levi, lat, uzm, vzm, vthzm, thzm):
     ntime = len(time)
-    nlevi = len(levi)
-    nlat = len(lat)
 
     latrad = (lat / 180.0) * numpy.pi
     coslat = numpy.cos(latrad)
     f = 2 * om * numpy.sin(latrad)
     vtem = vtem(time, levi, lat, vzm, vthzm, thzm)
 
-    utendvtem = numpy.zeros((ntime, nlevi, nlat), dtype='d')
+    utendvtem = numpy.zeros(uzm.shape, dtype='d')
     for itime in range(ntime):
-        ucos = numpy.einsum('ij,j->ij', uzm[itime, :, :], coslat)
+        ucos = numpy.einsum('ij...,j->ij...', uzm[itime, ...], coslat)
         dudphi = deriv(latrad, ucos, axis=1) / a
-        utendvtem[itime, :, :] = vtem[itime, :, :] * (f - dudphi)
+        utendvtem[itime, ...] = vtem[itime, ...] * (f - dudphi)
     return utendvtem
 
 
 def utendwtem(time, levi, lat, uzm, wzm, vthzm, thzm):
     ntime = len(time)
-    nlevi = len(levi)
-    nlat = len(lat)
 
     levi100 = 100.0 * levi
     wtem = _init_wtem(time, levi, lat, wzm, vthzm, thzm)
 
-    utendwtem = numpy.zeros((ntime, nlevi, nlat), dtype='d')
+    utendwtem = numpy.zeros(uzm.shape, dtype='d')
     for itime in range(ntime):
-        dudp = deriv(levi100, uzm[itime, :, :])
-        utendwtem[itime, :, :] = -1.0 * wtem[itime, :, :] * dudp
+        dudp = deriv(levi100, uzm[itime, ...])
+        utendwtem[itime, ...] = -1.0 * wtem[itime, ...] * dudp
     return utendwtem
 
 
 def _init_epfy(time, levi, lat, uzm, uvzm, vthzm, thzm):
     ntime = len(time)
-    nlevi = len(levi)
-    nlat = len(lat)
 
     latrad = (lat / 180.0) * numpy.pi
     coslat = numpy.cos(latrad)
     levi100 = 100.0 * levi
 
-    epfy = numpy.zeros((ntime, nlevi, nlat), dtype='d')
+    epfy = numpy.zeros(uzm.shape, dtype='d')
     for itime in range(ntime):
-        dthdp = deriv(levi100, thzm[itime, :, :])
-        dudp = deriv(levi100, uzm[itime, :, :])
-        psieddy = vthzm[itime, :, :] / dthdp
-        epfy[itime, :, :] = a * coslat * (dudp * psieddy - uvzm[itime, :, :])
+        dthdp = deriv(levi100, thzm[itime, ...])
+        dudp = deriv(levi100, uzm[itime, ...])
+        psieddy = vthzm[itime, ...] / dthdp
+        epfy[itime, ...] = a * coslat * (dudp * psieddy - uvzm[itime, ...])
     return epfy
 
 
 def epfy(time, levi, lat, uzm, uvzm, vthzm, thzm):
     levi100 = 100.0 * levi
     epfy = _init_epfy(time, levi, lat, uzm, uvzm, vthzm, thzm)
-    epfy = numpy.einsum('ijk,j->ijk', epfy, levi100) / p0
+    epfy = numpy.einsum('ij...,j->ij...', epfy, levi100) / p0
     return epfy
 
 
 def _init_epfz(time, levi, lat, uzm, uwzm, vthzm, thzm):
     ntime = len(time)
-    nlevi = len(levi)
-    nlat = len(lat)
 
     latrad = (lat / 180.0) * numpy.pi
     coslat = numpy.cos(latrad)
     f = 2 * om * numpy.sin(latrad)
     levi100 = 100.0 * levi
 
-    _uwzm = -1.0 * numpy.einsum('ijk,j->ijk', uwzm, levi100) / H
+    _uwzm = -1.0 * numpy.einsum('ij...,j->ij...', uwzm, levi100) / H
 
-    epfz = numpy.zeros((ntime, nlevi, nlat), dtype='d')
+    epfz = numpy.zeros(uzm.shape, dtype='d')
     for itime in range(ntime):
-        ucos = numpy.einsum('ij,j->ij', uzm[itime, :, :], coslat)
+        ucos = numpy.einsum('ij...,j->ij...', uzm[itime, ...], coslat)
         dudphi = deriv(latrad, ucos, axis=1) / a
-        dthdp = deriv(levi100, thzm[itime, :, :])
-        psieddy = vthzm[itime, :, :] / dthdp
-        epfz[itime, :, :] = a * coslat * \
-            (((f - dudphi) * psieddy) - _uwzm[itime, :, :])
+        dthdp = deriv(levi100, thzm[itime, ...])
+        psieddy = vthzm[itime, ...] / dthdp
+        epfz[itime, ...] = a * coslat * (((f - dudphi) * psieddy) - _uwzm[itime, ...])
     return epfz
 
 
@@ -148,8 +135,6 @@ def epfz(time, levi, lat, uzm, uwzm, vthzm, thzm):
 
 def utendepfd(time, levi, lat, uzm, uvzm, uwzm, vthzm, thzm):
     ntime = len(time)
-    nlevi = len(levi)
-    nlat = len(lat)
 
     latrad = (lat / 180.0) * numpy.pi
     coslat = numpy.cos(latrad)
@@ -158,36 +143,34 @@ def utendepfd(time, levi, lat, uzm, uvzm, uwzm, vthzm, thzm):
     epfy = _init_epfy(time, levi, lat, uzm, uvzm, vthzm, thzm)
     epfz = _init_epfz(time, levi, lat, uzm, uwzm, vthzm, thzm)
 
-    utendepfd = numpy.zeros((ntime, nlevi, nlat), dtype='d')
+    utendepfd = numpy.zeros(uzm.shape, dtype='d')
     for itime in range(ntime):
-        depfydphi = deriv(latrad, epfy[itime, :, :]
-                          * coslat, axis=1) / (a * coslat)
-        depfzdp = deriv(levi100, epfz[itime, :, :])
-        utendepfd[itime, :, :] = (depfydphi + depfzdp) / (a * coslat)
+        depfydphi = deriv(latrad, epfy[itime, ...] * coslat, axis=1) / (a * coslat)
+        depfzdp = deriv(levi100, epfz[itime, ...])
+        utendepfd[itime, ...] = (depfydphi + depfzdp) / (a * coslat)
     return utendepfd
 
 
 def psitem(time, levi, lat, vzm, vthzm, thzm):
     ntime = len(time)
     nlevi = len(levi)
-    nlat = len(lat)
 
     latrad = (lat / 180.0) * numpy.pi
     coslat = numpy.cos(latrad)
     levi100 = levi * 100.0
 
-    psitem = numpy.zeros((ntime, nlevi, nlat), dtype='d')
-    vzmwithzero = numpy.zeros((nlevi + 1, nlat), dtype='d')
+    psitem = numpy.zeros(vzm.shape, dtype='d')
+    vzm_shape_1 = list(vzm.shape[1:])
+    vzm_shape_1[0] += 1
+    vzmwithzero = numpy.zeros(vzm_shape_1, dtype='d')
     levi100withzero = numpy.zeros((nlevi + 1,), dtype='d')
     levi100withzero[1:] = levi100
     for itime in range(ntime):
-        dthdp = deriv(levi100, thzm[itime, :, :])
-        psieddy = vthzm[itime, :, :] / dthdp
-        vzmwithzero[1:, :] = vzm[itime, :, :]
+        dthdp = deriv(levi100, thzm[itime, ...])
+        psieddy = vthzm[itime, ...] / dthdp
+        vzmwithzero[1:, ...] = vzm[itime, ...]
         for ilev in range(1, nlevi + 1):
-            result = int_tabulated(
-                levi100withzero[0:ilev + 1], vzmwithzero[0:ilev + 1, :])
-            psitem[itime, ilev - 1,
-                   :] = ((2 * numpy.pi * a * coslat) / g0) * (result - psieddy[ilev - 1, :])
+            result = int_tabulated(levi100withzero[0:ilev + 1], vzmwithzero[0:ilev + 1, ...])
+            psitem[itime, ilev - 1, ...] = ((2 * numpy.pi * a * coslat) / g0) * (result - psieddy[ilev - 1, ...])
 
     return psitem
--- source/pyconform/modules/dynvarmipfunctions.py
+++ source/pyconform/modules/dynvarmipfunctions.py
@@ -17,109 +17,109 @@ import pyconform.modules.dynvarmipdiags as dvmd
 class DynVarMIPFunction(Function):
     key = 'dynvarmipfunc'
 
-    def __init__(self, *args):
-        super(DynVarMIPFunction, self).__init__(*args)
+    def __init__(self, time, lev, lat, *args):
+        super(DynVarMIPFunction, self).__init__(time, lev, lat, *args)
         self._dynvarmip_func = None
 
     def __getitem__(self, index):
-        args = [self.arguments[i][index] for i in len(self.arguments)]
+        args = [arg[index] for arg in self.arguments]
         arg_names = ','.join([arg.name for arg in args])
         data = self._dynvarmip_func(*args)
         name = '{}({})'.format(self.key, arg_names)
-        return PhysArray(data, name=name)
+        return PhysArray(data, name=name, dimensions=args[-1].dimensions)
 
 
 #=========================================================================
 # wtemDynVarMIPFunction
 #=========================================================================
-class wtemDynVarMIPFunction(Function):
+class wtemDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_wtem'
 
-    def __init__(self, time, levi, lat, wzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, wzm, vthzm, thzm):
         super(wtemDynVarMIPFunction, self).__init__(
-            time, levi, lat, wzm, vthzm, thzm)
+            time, lev, lat, wzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.wtem
 
 
 #=========================================================================
 # utendwtemDynVarMIPFunction
 #=========================================================================
-class utendwtemDynVarMIPFunction(Function):
+class utendwtemDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_utendwtem'
 
-    def __init__(self, time, levi, lat, uzm, wzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, uzm, wzm, vthzm, thzm):
         super(utendwtemDynVarMIPFunction, self).__init__(
-            time, levi, lat, uzm, wzm, vthzm, thzm)
+            time, lev, lat, uzm, wzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.utendwtem
 
 
 #=========================================================================
 # vtemDynVarMIPFunction
 #=========================================================================
-class vtemDynVarMIPFunction(Function):
+class vtemDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_vtem'
 
-    def __init__(self, time, levi, lat, vzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, vzm, vthzm, thzm):
         super(vtemDynVarMIPFunction, self).__init__(
-            time, levi, lat, vzm, vthzm, thzm)
+            time, lev, lat, vzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.vtem
 
 
 #=========================================================================
 # utendvtemDynVarMIPFunction
 #=========================================================================
-class utendvtemDynVarMIPFunction(Function):
+class utendvtemDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_utendvtem'
 
-    def __init__(self, time, levi, lat, uzm, vzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, uzm, vzm, vthzm, thzm):
         super(utendvtemDynVarMIPFunction, self).__init__(
-            time, levi, lat, uzm, vzm, vthzm, thzm)
+            time, lev, lat, uzm, vzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.utendvtem
 
 
 #=========================================================================
 # epfyDynVarMIPFunction
 #=========================================================================
-class epfyDynVarMIPFunction(Function):
+class epfyDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_epfy'
 
-    def __init__(self, time, levi, lat, uzm, uvzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, uzm, uvzm, vthzm, thzm):
         super(epfyDynVarMIPFunction, self).__init__(
-            time, levi, lat, uzm, uvzm, vthzm, thzm)
+            time, lev, lat, uzm, uvzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.epfy
 
 
 #=========================================================================
 # epfzDynVarMIPFunction
 #=========================================================================
-class epfzDynVarMIPFunction(Function):
+class epfzDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_epfz'
 
-    def __init__(self, time, levi, lat, uzm, uwzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, uzm, uwzm, vthzm, thzm):
         super(epfzDynVarMIPFunction, self).__init__(
-            time, levi, lat, uzm, uwzm, vthzm, thzm)
+            time, lev, lat, uzm, uwzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.epfz
 
 
 #=========================================================================
 # utendepfdDynVarMIPFunction
 #=========================================================================
-class utendepfdDynVarMIPFunction(Function):
+class utendepfdDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_utendepfd'
 
-    def __init__(self, time, levi, lat, uzm, uvzm, uwzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, uzm, uvzm, uwzm, vthzm, thzm):
         super(utendepfdDynVarMIPFunction, self).__init__(
-            time, levi, lat, uzm, uvzm, uwzm, vthzm, thzm)
+            time, lev, lat, uzm, uvzm, uwzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.utendepfd
 
 
 #=========================================================================
 # psitemDynVarMIPFunction
 #=========================================================================
-class psitemDynVarMIPFunction(Function):
+class psitemDynVarMIPFunction(DynVarMIPFunction):
     key = 'dynvarmip_psitem'
 
-    def __init__(self, time, levi, lat, vzm, vthzm, thzm):
+    def __init__(self, time, lev, lat, vzm, vthzm, thzm):
         super(psitemDynVarMIPFunction, self).__init__(
-            time, levi, lat, vzm, vthzm, thzm)
+            time, lev, lat, vzm, vthzm, thzm)
         self._dynvarmip_func = dvmd.psitem
