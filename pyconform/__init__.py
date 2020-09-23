70910db98c2281118635926ab1eb0dd8088532f4
Kevin Paul <kpaul@ucar.edu>
2020-09-23 15:02:00 -0600

    Remove version import

--- pyconform/__init__.py
+++ pyconform/__init__.py
@@ -4,12 +4,9 @@ PyConform
 A package for conforming a NetCDF dataset for publication
 
 :AUTHORS: Sheri Mickelson, Kevin Paul
-:COPYRIGHT: 2017, University Corporation for Atmospheric Research
+:COPYRIGHT: 2017-2020, University Corporation for Atmospheric Research
 :LICENSE: See the LICENSE.rst file for details
 
 Send questions and comments to Kevin Paul (kpaul@ucar.edu) or
 Sheri Mickelson (mickelso@ucar.edu)
 """
-
-from version import __version__
-
